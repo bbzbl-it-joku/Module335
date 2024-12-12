6f70d6e45299a9d71a72ed3c7acae7e95d65c616
jokudev <git@joku.dev>
2024-12-12 14:31:41 +0100

    feat: add Google Maps API key to environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://mtpwxggfsztqncdtqmxd.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10cHd4Z2dmc3p0cW5jZHRxbXhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3MzcxNjQsImV4cCI6MjA0OTMxMzE2NH0.XTHJa--Ex0CLZLyfljWjEtrqIYsmfaajkcJnq2MVQ-U',
+  googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
 };
 
 /*
